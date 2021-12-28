de0bbf9f1a6ffdfbb7fdcbe69b2238ae5702c5d7
Jürgen Bayer <jb@juergen-bayer.net>
2021-12-28 19:24:53 +0000

    Add demo for displaying validation messages individually

---
+++ BlazorBasics.Client/Shared/EditDemoDisplayingInvalidInputIndividually.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using BlazorBasics.Shared;
+using Microsoft.JSInterop;
+
+namespace BlazorBasics.Client.Shared;
+
+public partial class EditDemoDisplayingInvalidInputIndividually
+{
+    private readonly Starship starship = new() { ProductionDate = DateTime.UtcNow };
+
+    private void HandleValidSubmit()
+    {
+        this.logger.LogInformation("handleValidSubmit was called");
+        
+        // TODO: Process the valid form
+        var jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true };
+        var starshipJson = JsonSerializer.Serialize(this.starship, jsonSerializerOptions);
+        this.logger.LogInformation(starshipJson);
+    }
+}
